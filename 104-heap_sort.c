8e2dbf28e902a9db1c4447903630f19316badbf3
Akanvar <kelvinawiapo9@gmail.com>
2024-01-24 01:35:24 +0000

    modified

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,5 +1,19 @@
 #include "sort.h"
 /**
+ * swap_int - Swaps two integers in an array
+ *
+ * @a: integer
+ * @b: integer
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+/**
  * heap_sort - sorts an array of integers in ascending order
  * using the sift-down heap sort algorithm
  *
